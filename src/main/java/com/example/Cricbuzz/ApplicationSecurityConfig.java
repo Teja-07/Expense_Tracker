e7cabcbe780803b3fa91dd321ae98271e0a966d0
Teja chinitha <82213085+Teja-07@users.noreply.github.com>
2024-04-29 13:01:58 +0530

    Update ApplicationSecurityConfig.java

--- src/main/java/com/example/Cricbuzz/ApplicationSecurityConfig.java
+++ src/main/java/com/example/Cricbuzz/ApplicationSecurityConfig.java
@@ -16,5 +16,20 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity  
 public class ApplicationSecurityConfig {
-	
+	@Bean
+	protected InMemoryUserDetailsManager configAuthentication() {
+		List<UserDetails> list=new ArrayList<>();
+		UserDetails admin= new User("names", GrantedAuthority);
+		list.add(admin);
+		return new InMemoryUserDetailsManager(list);
+	}
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		//declares which Page(URL) will have What access type
+		http.authorizeHttpRequests()
+		.requestMatchers("/home").permitAll()
+		
+		return http.build();
+	}
 }
